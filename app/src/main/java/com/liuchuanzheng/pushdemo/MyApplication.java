685bc1c1511a1ace1379dd36453f9eb932e364f4
liuchuanzheng <1052374416@qq.com>
2019-05-24 16:39:39 +0800

    Initial commit

---
+++ app/src/main/java/com/liuchuanzheng/pushdemo/MyApplication.java
@@ -0,0 +1,106 @@
+package com.liuchuanzheng.pushdemo;
+
+import android.app.Application;
+import android.app.Notification;
+import android.content.Context;
+import android.util.Log;
+import android.widget.RemoteViews;
+import android.widget.Toast;
+
+import com.umeng.commonsdk.UMConfigure;
+import com.umeng.message.IUmengRegisterCallback;
+import com.umeng.message.PushAgent;
+import com.umeng.message.UmengMessageHandler;
+import com.umeng.message.UmengNotificationClickHandler;
+import com.umeng.message.entity.UMessage;
+
+/**
+ * @author 刘传政
+ * @date 2019-05-24 15:37
+ * QQ:1052374416
+ * 电话:18501231486
+ * 作用:
+ * 注意事项:
+ */
+public class MyApplication extends Application {
+    private static final String TAG = "liuchuanzheng";
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        // 在此处调用基础组件包提供的初始化函数 相应信息可在应用管理 -> 应用信息 中找到 http://message.umeng.com/list/apps
+        // 参数一：当前上下文context；
+        // 参数二：应用申请的Appkey（需替换）；
+        // 参数三：渠道名称；
+        // 参数四：设备类型，必须参数，传参数为UMConfigure.DEVICE_TYPE_PHONE则表示手机；传参数为UMConfigure.DEVICE_TYPE_BOX则表示盒子；默认为手机；
+        // 参数五：Push推送业务的secret 填充Umeng Message Secret对应信息（需替换）
+        UMConfigure.init(this, "5ce79cd73fc195405c000018", "Umeng",
+                UMConfigure.DEVICE_TYPE_PHONE, "3e2e164db9b64201fe0409d28081ec15");
+
+
+        //获取消息推送代理示例
+        PushAgent mPushAgent = PushAgent.getInstance(this);
+        //注册推送服务，每次调用register方法都会回调该接口
+        mPushAgent.register(new IUmengRegisterCallback() {
+
+            @Override
+            public void onSuccess(String deviceToken) {
+                //注册成功会返回deviceToken deviceToken是推送消息的唯一标志
+                Log.i(TAG,"注册成功：deviceToken：-------->  " + deviceToken);
+            }
+
+            @Override
+            public void onFailure(String s, String s1) {
+                Log.e(TAG,"注册失败：-------->  " + "s:" + s + ",s1:" + s1);
+            }
+        });
+
+
+        UmengMessageHandler umengMessageHandler = new UmengMessageHandler(){
+            /**
+             * 自定义通知栏样式的回调方法
+             */
+            @Override
+            public Notification getNotification(Context context, UMessage msg) {
+                Log.i(TAG,"收到友盟推送"+msg.getRaw().toString());
+                switch (msg.builder_id) {
+                    case 1:
+                        Notification.Builder builder = new Notification.Builder(context);
+                        RemoteViews myNotificationView = new RemoteViews(context.getPackageName(),
+                                R.layout.notification_view);
+                        myNotificationView.setTextViewText(R.id.notification_title, msg.title);
+                        myNotificationView.setTextViewText(R.id.notification_text, msg.text);
+//                        myNotificationView.setImageViewBitmap(R.id.notification_large_icon, getLargeIcon(context, msg));
+                        myNotificationView.setImageViewResource(R.id.notification_small_icon,
+                                getSmallIconId(context, msg));
+                        builder.setContent(myNotificationView)
+                                .setSmallIcon(getSmallIconId(context, msg))
+                                .setTicker(msg.ticker)
+                                .setAutoCancel(true);
+
+                        return builder.getNotification();
+                    default:
+                        //默认为0，若填写的builder_id并不存在，也使用默认。
+                        return super.getNotification(context, msg);
+                }
+            }
+
+            @Override
+            public void dealWithCustomMessage(Context context, UMessage uMessage) {
+                //自定义消息。不是通知类型的推送。
+                Log.i(TAG,"收到自定义消息"+uMessage.getRaw().toString());
+                super.dealWithCustomMessage(context, uMessage);
+            }
+        };
+        mPushAgent.setMessageHandler(umengMessageHandler);
+        UmengNotificationClickHandler notificationClickHandler = new UmengNotificationClickHandler() {
+
+            @Override
+            public void dealWithCustomAction(Context context, UMessage msg) {
+                //这里处理的是自定义行为。
+                Toast.makeText(context, msg.custom, Toast.LENGTH_LONG).show();
+                Log.i(TAG,"click");
+            }
+        };
+        mPushAgent.setNotificationClickHandler(notificationClickHandler);
+    }
+}
